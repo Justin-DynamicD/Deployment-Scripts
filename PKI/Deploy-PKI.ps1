3899900a6fd16c39a2cba81abbb199423567f74f
Justin-DynamicD <justin.king@agile360.com>
2015-12-22 17:33:06 -0800

    Initial Push
    Reconstructed Scripts folder to make it easier to grab only needed files

---
+++ PKI/Deploy-PKI.ps1
@@ -0,0 +1,166 @@
+﻿param (
+    [Parameter(Mandatory=$true)][String][ValidateSet("Root","Issue")]$Role,
+    [Parameter(Mandatory=$true)][String]$FQDN = "pki.contoso.com",
+    [Parameter(Mandatory=$true)][String]$CAName,
+    [Parameter(Mandatory=$false)][String]$DomainDN,
+    [Parameter(Mandatory=$false)][Int][ValidateSet(1,2)]$IssueStep = 1,
+    [Parameter(Mandatory=$false)][Switch]$CreateDeploymentZIP = $true,
+    [Parameter(Mandatory=$false)][Switch]$DeployZIP = $false,
+    [Parameter(Mandatory=$false)][Switch]$DeployZIPAD = $false
+    )
+
+Write-Verbose "Importing ServerManager is it's not loaded ..."
+If (!(Get-Module ServerManager)){
+    Import-Module ServerManager
+    }
+
+#Only process installs if we are on step 1
+If ($IssueStep -eq 1) {
+
+    #Select Template Based on $Role
+    Switch ($Role) {
+        Issue {$catemplate="Issue-CAPolicy.inf"}
+        Root {$catemplate="Root-CAPolicy.inf"}
+        }
+
+    #Generate CAPolicy.inf from templates    Write-Verbose "Creating capolicy.inf file ..."    (Get-Content $catemplate).replace('[FQDN]',$FQDN) | Set-Content $env:SystemRoot\CAPolicy.inf -force
+    #Install and configure core features
+    Write-Verbose "Installing required features..."
+    Switch ($Role) {
+        Issue {
+            Add-WindowsFeature ADCS-Cert-Authority,ADCS-Web-Enrollment -IncludeManagementTools
+            Install-ADcsCertificationAuthority -CACommonName $CAName -CAType EnterpriseSubordinateCA -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" -Force
+            }
+        Root {
+            Add-WindowsFeature ADCS-Cert-Authority -IncludeManagementTools
+            Install-ADcsCertificationAuthority -CACommonName $CAName -CAType StandaloneRootCA -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" -HashAlgorithmName SHA256 -ValidityPeriod Years -ValidityPeriodUnits 20 -Force
+            }
+        } #end switch
+    } # End "IssueStep 1" Condition
+
+#Configure the Root Server
+If ($Role -eq "Root") {
+    
+    #Set the registry information on the certificate service
+    Write-Verbose "Setting the registry information..."
+    $cmd = 'certutil.exe -setreg ca\DSConfigDN "CN=Configuration,'+$DomainDN+'"'
+    Invoke-Expression $cmd | Write-Verbose
+
+    #Configure the CRL, CDP and CA Publication URLs
+    Write-Verbose "Setting publication URLs..."
+    $cmd = 'certutil.exe -setreg CA\CRLPublicationURLs "1:'+$env:SystemRoot+'\system32\CertSrv\CertEnroll\%3%8%9.crl\n2:http://'+$FQDN+'/pki/%3%8%9.crl"'
+    Invoke-Expression $cmd | Write-Verbose
+    $cmd = 'certutil.exe -setreg CA\CACertPublicationURLs "1:'+$env:SystemRoot+'\system32\CertSrv\CertEnroll\%1_%3%4.crt\n2:http://'+$FQDN+'/pki/%1_%3%4.crt"'
+    Invoke-Expression $cmd | Write-Verbose
+
+    #Configure the CRL Validity Period
+    Write-Verbose "Configure the CRL Validity Period..."    $cmd = 'certutil.exe -setreg ca\ValidityPeriodUnits 10'    Invoke-Expression $cmd | Write-Verbose    $cmd = 'certutil.exe -setreg ca\ValidityPeriod "Years"'    Invoke-Expression $cmd | Write-Verbose
+
+    #Enable Auditing
+    Write-Verbose "Enable Auditing ..."
+    $cmd = 'certutil.exe -setreg ca\AuditFilter 127'    Invoke-Expression $cmd | Write-Verbose
+
+    #Resart Services to apply
+    write-verbose "Restarting services to apply changes ..."
+    restart-service certsvc
+
+    #Exporting Information
+    Write-Verbose "Exporting Information..."
+    $cmd = 'certutil.exe -CRL'    Invoke-Expression $cmd | Write-Verbose
+
+    #Create a ZIP of certificate files if switch is set
+    IF ($CreateDeploymentZIP) {
+        Write-Verbose "Creating a ZIP of certificate files..."
+        $source = $env:SystemRoot+'\system32\CertSrv\CertEnroll'
+        $Destination = $env:SystemDrive+'\root-certificates.zip'
+        If(Test-path $destination) {Remove-item $destination}
+        Add-Type -assembly "system.io.compression.filesystem"
+        [io.compression.zipfile]::CreateFromDirectory($Source, $destination) 
+        } # End Zip variable
+    } # End Root
+
+#If switch $DeployZIPAD is set and we are not in workgroup mode, push all certificates in the zip to AD
+If (($DeployZIPAD) -and ($env:Userdomain -eq $env:COMPUTERNAME)) {Write-Error "cannot publish to AD from a workgroup computer"}
+If (($DeployZIPAD) -and ($env:Userdomain -ne $env:COMPUTERNAME)) {
+    
+    #Unzip all the certificates to a temp directory
+    $Source = $env:SystemDrive+'\root-certificates.zip'
+    $Destination = $env:SystemDrive+'\temp\certs'
+    If(Test-path $destination) {Remove-item $destination -recurse}
+    Add-Type -assembly "system.io.compression.filesystem"
+    [io.compression.zipfile]::ExtractToDirectory($Source, $destination)
+
+    #publish each Cert to AD
+    $certs = (Get-ChildItem -Path $Destination\* -Include *.crt).Name
+    $CRLs = (Get-ChildItem -Path $Destination\* -Include *.crl).Name
+    For-each $Certfile in $certs {
+        $cmd = 'certutil.exe -dspublish -f "'+$Destination+'\'+$certfile+'" RootCA'        Invoke-Expression $cmd | Write-Verbose
+        $cmd = 'certutil.exe -addstore -f root "'+$Destination+'\'+$certfile+'"'        Invoke-Expression $cmd | Write-Verbose
+        }
+     For-each $CRLfile in $CRLs {
+        $cmd = 'certutil.exe -addstore -f root "'+$Destination+'\'+$CRLfile+'"'        Invoke-Expression $cmd | Write-Verbose
+        }
+
+    #cleanup temp directory
+    If(Test-path $destination) {Remove-item $destination -recurse}
+
+    } #end AD Deployment
+
+#Configure the Issueing Server on Step 2
+If (($Role -eq "Issue") -and ($IssueStep -eq 2)) {
+    
+    #Configure the WebEnrollment service
+    Install-AdcsWebEnrollment -Force
+
+    #If DeployZip is set, unzip files to CertErnoll
+    If ($DeployZIP) {
+        $Source = $env:SystemDrive+'\root-certificates.zip'
+        $Destination = $env:SystemRoot+'\System32\certsrv\CertEnroll'
+        Add-Type -assembly "system.io.compression.filesystem"
+        [io.compression.zipfile]::ExtractToDirectory($Source, $destination)
+        }
+
+    #Create PKI share directory and populate
+    Write-Verbose "Creating new $env:SystemDrive\PKI ..."
+    new-item -ItemType Directory $env:SystemDrive\PKI\Policy -force
+    Copy-Item $env:SystemRoot\System32\certsrv\CertEnroll\* $env:SystemDrive\PKI -Force
+    Write-Output "Legal Policy." | Out-File $env:SystemDrive\PKI\Policy\USLegalPolicy.asp
+    Write-Output "Limited Use Policy." | Out-File $env:SystemDrive\PKI\Policy\USLimitedUsePolicy.asp
+
+    #Update ACLs on PKI Folder
+    $ACL = (Get-item $env:SystemDrive\PKI).GetAccessControl('Access')
+    $AR = New-Object System.Security.AccessControl.FileSystemAccessRule('BUILTIN\IIS_IUSRS', 'ReadandExecute', 'ContainerInherit,ObjectInherit', 'None', 'Allow')
+    $ACL.SetAccessRule($AR)
+    Set-Acl -path $env:SystemDrive\PKI -AclObject $ACL
+
+    #Create the Virtual Directory
+    if (!(Get-WebVirtualDirectory pki)) {
+        New-WebVirtualDirectory -Name pki -PhysicalPath $env:SystemDrive\PKI -Site "Default Web Site"
+        }
+
+    #Set Hash Algorithm to SHA256
+    Write-Verbose "Set Hash Algorithm to SHA256..."    $cmd = 'certutil.exe -setreg ca\csp\CNGHashAlgorithm SHA256'    Invoke-Expression $cmd | Write-Verbose
+
+    #Configure the CRL, CDP and CA Publication URLs
+    Write-Verbose "Setting publication URLs..."
+    $cmd = 'certutil.exe -setreg CA\CRLPublicationURLs "1:'+$env:SystemRoot+'\system32\CertSrv\CertEnroll\%3%8%9.crl\n2:http://'+$FQDN+'/pki/%3%8%9.crl\n1:file://'+$env:SystemDrive+'\pki\%3%8%9.crl"'
+    Invoke-Expression $cmd | Write-Verbose
+    $cmd = 'certutil.exe -setreg CA\CACertPublicationURLs "1:'+$env:SystemRoot+'\system32\CertSrv\CertEnroll\%1_%3%4.crt\n2:http://'+$FQDN+'/pki/%1_%3%4.crt\n1:file://'+$env:SystemDrive+'\pki\%3%8%9.crl"'
+    Invoke-Expression $cmd | Write-Verbose
+
+    #Configure the CRL Validity Period
+    Write-Verbose "Configure the CRL Validity Period..."    $cmd = 'certutil.exe -setreg ca\ValidityPeriodUnits 10'    Invoke-Expression $cmd | Write-Verbose    $cmd = 'certutil.exe -setreg ca\ValidityPeriod "Years"'    Invoke-Expression $cmd | Write-Verbose
+
+    #Enable Auditing
+    Write-Verbose "Enable Auditing ..."
+    $cmd = 'certutil.exe -setreg ca\AuditFilter 127'    Invoke-Expression $cmd | Write-Verbose
+
+    #Resart Services to apply
+    write-verbose "Restarting services to apply changes ..."
+    restart-service certsvc
+
+    #Exporting Information
+    Write-Verbose "Exporting Information..."
+    $cmd = 'certutil.exe -CRL'    Invoke-Expression $cmd | Write-Verbose
+
+    }
---
+++ SQL_2014/Deploy-SQL.psm1
@@ -0,0 +1,146 @@
+﻿######################################################################################
+# The Get-TargetResource cmdlet.
+# This function will get the present SQL instances on the system
+######################################################################################
+function Get-TargetResource
+{
+[CmdletBinding()][OutputType([System.Collections.Hashtable])]
+param (
+    [Parameter(Mandatory=$false)][String]$WinSources,
+    [Parameter(Mandatory=$false)][String]$ISOImage,
+    [Parameter(Mandatory=$false)][String]$ConfigurationFile
+    )
+ 
+    $sqlInstances = gwmi win32_service -computerName localhost | ? { $_.Name -match "mssql*" -and $_.PathName -match "sqlservr.exe" } | % { $_.Caption }
+    $res = $sqlInstances -ne $null -and $sqlInstances -gt 0
+    $vals = @{
+        Installed = $res;
+        InstanceCount = $sqlInstances.count
+        }
+    $vals
+ } #End Function
+
+
+######################################################################################
+# The Test-TargetResource cmdlet.
+# This replies a $true or $false for each setting
+######################################################################################
+function Test-TargetResource
+{
+[CmdletBinding()][OutputType([System.Boolean])]
+param (
+    [Parameter(Mandatory=$false)][String]$WinSources,
+    [Parameter(Mandatory=$false)][String]$ISOImage,
+    [Parameter(Mandatory=$false)][String]$ConfigurationFile
+    )
+
+ValidateProperties @PSBoundParameters
+
+   } #End Function
+
+
+######################################################################################
+# The Set-TargetResource cmdlet.
+# This Applies all variables to the install script by adding "-apply"
+######################################################################################
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][String]$WinSources = "\\FS-01\Deployment\Server_2012R2\Sources\SXS",
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][String]$ISOImage = "\\FS-01\Deployment\SQL_2014\sql_server_2014_standard.iso",
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][String]$ConfigurationFile = "\\FS-01\Deployment\SQL_2014\SQL-Configuration.ini",
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][String]$SQLSVCPASSWORD,
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][String]$AGTSVCPASSWORD
+        )
+
+    ValidateProperties @PSBoundParameters -Apply
+
+   } #End Function
+
+
+#######################################################################################
+#  Helper function that validates the SQL Install. If the switch parameter
+# "Apply" is set, then it will set the properties after a test
+#######################################################################################
+function ValidateProperties
+{
+    param (
+        [Parameter(Mandatory=$false)][String]$WinSources,
+        [Parameter(Mandatory=$false)][String]$ISOImage,
+        [Parameter(Mandatory=$false)][String]$ConfigurationFile,
+        [Parameter(Mandatory=$false)][String]$SQLSVCPASSWORD,
+        [Parameter(Mandatory=$false)][String]$AGTSVCPASSWORD,
+        [Parameter(Mandatory=$false)][Switch]$Apply
+        )
+
+    #Start assuming the configuration will be valid.
+    [boolean]$ConfigurationValid = $true
+
+    #Set staging directory for disk with the most free space
+    $StagingPath = (get-volume |sort-object SizeRemaining -Descending)[0].Driveletter + ':\temp'
+
+    #Test if all .Net requirements are met
+    If (((Get-WindowsFeature Net-Framework-Core).InstallState) -ne "Installed") {
+        Write-Verbose ".Net 3.5 Is not Installed"
+        If($Apply){
+            Install-WindowsFeature Net-Framework-Core -Source $winsources
+            }
+        Else {$ConfigurationValid = $false}
+        } #End 3.5 Check
+    If (((Get-WindowsFeature Net-Framework-Core).InstallState) -ne "Installed") {
+        Write-Verbose ".Net 4.5 Is not Installed"
+        If($Apply){
+            Install-WindowsFeature Net-Framework-45-Core
+            }
+        Else {$ConfigurationValid = $false}
+        } #End 45 Check
+
+    #If Apply is set, stage ISO and unattend files to local path
+    If ($Apply) {
+        if (!(test-path $StagingPath)) {new-item -ItemType Directory $StagingPath | Out-Null}
+        if (!(test-path $StagingPath\SQLServer.iso)) {copy-item $ISOImage $StagingPath\SQLServer.iso -Force | Out-Null}
+        if (!(test-path $StagingPath\ConfigurationFile.ini)) {copy-item $ConfigurationFile $StagingPath\ConfigurationFile.ini -Force | Out-Null}
+        }
+
+    #Check for SQL Installation
+    $sqlInstances = gwmi win32_service -computerName localhost | ? { $_.Name -match "mssql*" -and $_.PathName -match "sqlservr.exe" } | % { $_.Caption }
+    $res = $sqlInstances -ne $null -and $sqlInstances -gt 0
+
+    #Take Action based on $RES being present
+    if ($res) {
+        Write-Verbose "SQL Server is already installed"
+        }
+    else {
+        if ($apply) {
+            Write-Verbose "Installing SQL..."
+            # mount the iso
+            $setupDriveLetter = (Mount-DiskImage -ImagePath $StagingPath\SQLServer.iso -PassThru | Get-Volume).DriveLetter
+            if ($setupDriveLetter -eq $null) {
+                throw "Could not mount SQL install iso"
+                }
+            Write-Verbose "Drive letter for iso is: $setupDriveLetter"
+                 
+            # run the installer using the ini file
+            $cmd = $setupDriveLetter + ':\Setup.exe /ConfigurationFile='+$StagingPath+'\ConfigurationFile.ini /SQLSVCPASSWORD="'+$SQLSVCPASSWORD+'" /AGTSVCPASSWORD="'+$AGTSVCPASSWORD+'" /IAcceptSQLServerLicenseTerms'
+            Write-Verbose "Running SQL Install - check %programfiles%\Microsoft SQL Server\120\Setup Bootstrap\Log\ for logs..."
+            Invoke-Expression $cmd | Write-Verbose
+
+            #unmount the ISO when complete
+            Dismount-DiskImage -ImagePath $StagingPath\SQLServer.iso -PassThru
+
+            } #End SQL Install
+        else { $ConfigurationValid = $false}
+        } #End Nest If Function for install vs report
+
+    #if apply got this far without errors, remove the staged files
+    If (($Apply) -and ($ConfigurationValid)) {
+        if (test-path $StagingPath\SQLServer.iso) {remove-item $StagingPath\SQLServer.iso -Force | Out-Null}
+        if (test-path $StagingPath\ConfigurationFile.ini) {remove-item $StagingPath\ConfigurationFile.ini -Force | Out-Null}
+        }
+
+    #if this is a test only, report back compliance boolean
+    If (!($Apply)) {
+        return $ConfigurationValid
+        }
+   } #End Function
